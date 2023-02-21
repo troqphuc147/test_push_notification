5072a1e2176a166179a7273ede88e4830f8a307e
troqphuc147 <phuchuynh020601@gmail.com>
2023-02-21 15:09:09 +0700

    local notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:push_noti/show_message_data_screen.dart';
+
+import 'gettoken.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+final StreamController<String?> selectNotificationStream =
+    StreamController<String?>.broadcast();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
 
   debugPrint("Handling a background message: ${message.messageId}");
+}
+
+void notificationTapBackground(NotificationResponse notificationResponse) {
+  // ignore: avoid_print
+  print('notification(${notificationResponse.id}) action tapped: '
+      '${notificationResponse.actionId} with'
+      ' payload: ${notificationResponse.payload}');
+  if (notificationResponse.input?.isNotEmpty ?? false) {
+    // ignore: avoid_print
+    print(
+        'notification action tapped with input: ${notificationResponse.input}');
+  }
 }
 
 Future<void> main() async {
@@ -28,7 +51,28 @@ Future<void> main() async {
 
   debugPrint('User granted permission: ${settings.authorizationStatus}');
 
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: AndroidInitializationSettings('ic_launcher'),
+  );
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse:
+        (NotificationResponse notificationResponse) {
+      switch (notificationResponse.notificationResponseType) {
+        case NotificationResponseType.selectedNotification:
+          selectNotificationStream.add(notificationResponse.payload);
+          break;
+        case NotificationResponseType.selectedNotificationAction:
+          if (notificationResponse.actionId == '0') {
+            selectNotificationStream.add(notificationResponse.payload);
+          }
+          break;
+      }
+    },
+    onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
+  );
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
     debugPrint('Got a message whilst in the foreground!');
     debugPrint('Message data: ${message.data}');
 
@@ -36,6 +80,17 @@ Future<void> main() async {
       debugPrint(
           'Message also contained a notification: ${message.notification}');
     }
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('Day la channel id', 'your channel name',
+            channelDescription: 'ynojjsb',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'Test remote notification', 'Khi trong app', notificationDetails,
+        payload: message.data.toString());
   });
 
   runApp(const MyApp());
@@ -81,8 +136,9 @@ class _MyHomePageState extends State<MyHomePage> {
     FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
   }
 
-  void _handleMessage(RemoteMessage message) {
+  Future<void> _handleMessage(RemoteMessage message) async {
     if (message.data['type'] == 'test') {
+      // ignore: use_build_context_synchronously
       Navigator.pushNamed(
         context,
         ShowMessageDataScreen.routeName,
@@ -95,6 +151,44 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _setUpInteractedMessage();
+    _requestPermission();
+    _configureSelectNotificationSubject();
+  }
+
+  _requestPermission() async {
+    final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
+        flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>();
+
+    final bool? granted = await androidImplementation?.requestPermission();
+  }
+
+  void _configureSelectNotificationSubject() {
+    selectNotificationStream.stream.listen((String? payload) async {
+      await Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) => const ShowMessageDataScreen(),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    selectNotificationStream.close();
+    super.dispose();
+  }
+
+  Future<void> _showNotification() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('Day la channel id', 'your channel name',
+            channelDescription: 'ynojjsb',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'Test local notification', 'Này là body', notificationDetails,
+        payload: '"type": "test"');
   }
 
   @override
@@ -114,9 +208,34 @@ class _MyHomePageState extends State<MyHomePage> {
               'Hi',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                print(await getFcmToken());
+                await _showNotification();
+              },
+              child: Text(
+                'Show local notification',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
 }
